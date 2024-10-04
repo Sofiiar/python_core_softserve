f78dec40be4fc039e5919bbe74badf902bc8785c
Sofiiar <sofiiaradul@gmail.com>
2024-10-04 11:32:07 +0200

    PY-1: updated to use pytest

--- homework_1/test_sorted.py
+++ homework_1/test_sorted.py
@@ -1,16 +1,14 @@
-import unittest
+def test_sorted_default():
+    assert sorted(["banana", "ball", "cat"]) == ["ball", "banana", "cat"]
 
 
-class TestSortedFunctions(unittest.TestCase):
+def test_sorted_by_length():
+    assert sorted(["banana", "ball", "cat"], key=len) == ["cat", "ball", "banana"]
 
-    def test_sorted_default(self):
-        self.assertEqual(sorted(["banana", "ball", "cat"]), ["ball", "banana", "cat"])
 
-    def test_sorted_by_length(self):
-        self.assertEqual(sorted(["banana", "ball", "cat"], key=len), ["cat", "ball", "banana"])
+def test_sorted_reverse():
+    assert sorted(["banana", "ball", "cat"], reverse=True) == ["cat", "banana", "ball"]
 
-    def test_sorted_reverse(self):
-        self.assertEqual(sorted(["banana", "ball", "cat"], reverse=True), ["cat", "banana", "ball"])
 
-    def test_sorted_reverse_by_length(self):
-        self.assertEqual(sorted(["banana", "ball", "cat"], reverse=True, key=len), ["banana", "ball", "cat"])
+def test_sorted_reverse_by_length():
+    assert sorted(["banana", "ball", "cat"], reverse=True, key=len) == ["banana", "ball", "cat"]
