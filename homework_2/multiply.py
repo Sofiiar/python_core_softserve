dcf1bf51c5e882710aee4f1a4b0fb46cbe4c4b9d
Sofiiar <sofiiaradul@gmail.com>
2024-10-06 13:48:58 +0200

    PY-2: updated quotes for tasks desriptions

--- homework_2/multiply.py
+++ homework_2/multiply.py
@@ -1,4 +1,6 @@
-#  Write a function that will receive 2 numbers as input and it should return the multiplication of these 2 numbers.
+"""
+Write a function that will receive 2 numbers as input and it should return the multiplication of these 2 numbers.
+"""
 
 
 def mult_two(arg1, arg2):
--- homework_2/three_words.py
+++ homework_2/three_words.py
@@ -1,7 +1,8 @@
-# You are given a string with words and numbers separated by whitespaces (one space).
-# The words contains only letters. You should check if the string contains three words in succession.
-# For example, the string "start 5 one two three 7 end" contains three words in succession.
-
+"""
+You are given a string with words and numbers separated by whitespaces (one space).
+The words contains only letters. You should check if the string contains three words in succession.
+For example, the string "start 5 one two three 7 end" contains three words in succession.
+"""
 
 def three_words(txt):
     words = txt.split()
